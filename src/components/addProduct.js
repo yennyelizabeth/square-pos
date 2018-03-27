145771d114425d306c4474f3328f3466efc62009
kamalyzl <kamaly.kazal@gmail.com>
2018-03-27 12:32:17 -0500

    delete compenets innecesary

--- src/components/addProduct.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const AddProducts = () => (
-    <div>Tarjeta</div>
-)
-
-export default AddProducts
--- src/components/report.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Report = () => (
-    <div>Report</div>
-)
-
-export default Report

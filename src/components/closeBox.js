5948ea7c525fbceffb7407d974819bf08c222c28
katherinelef <katherine.lef@hotmail.com>
2018-03-28 10:50:38 -0500

    cambios finales

--- src/components/closeBox.js
+++ src/components/closeBox.js
@@ -4,43 +4,43 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 import './main.css';
 
 const CloseBox = props => (
-    <div class="container form-pay">
-      <div class="row">
-        <p className = "text-left date">{new Date().toLocaleDateString() }</p>
-        <i class="far fa-calendar-alt"></i>
-        <div className="col-12 monto-pay">
-          <p>CIERRE DE CAJA</p>
-          <div>
-            {`S/ ${localStorage.total}.00`}
-          </div>
-        </div>
-        <div className="col-12 monto-pay">
-          
-        <form class="form-inline">
-  <div class="form-group">
-    <label for="inputPassword6">Saldo Inicial de Caja</label>
-    <input type="number" id="inputPassword6" class="form-control mx-sm-3" aria-describedby="passwordHelpInline"/>
-</div>
-<div class="form-group">
-    <label for="inputPassword6">Ventas en efectivo</label>
-    <input type="number" id="inputPassword6" class="form-control mx-sm-3" aria-describedby="passwordHelpInline"/>
-</div>
-<div class="form-group">
-    <label for="inputPassword6">Saldo de Caja</label>
-    <input type="number" id="inputPassword6" class="form-control mx-sm-3" aria-describedby="passwordHelpInline"/>
-</div>
-<div class="form-group">
-    <label for="inputPassword6">Ventas con Tarjeta</label>
-    <input type="number" id="inputPassword6" class="form-control mx-sm-3" aria-describedby="passwordHelpInline"/>
-</div>
-</form>
-        </div>
+    <div className="container form-pay">
+        <div className="row">
+            <p className="text-left date">{new Date().toLocaleDateString()}</p>
+            <i className="far fa-calendar-alt"></i>
+            <div className="col-12 monto-pay">
+                <p>CIERRE DE CAJA</p>
+                <div>
+                    {`S/ ${localStorage.total}.00`}
+                </div>
+            </div>
+            <div className="col-12 monto-pay">
 
-        <button type="button" className="btn btn-primary btn-lg caja">Cerrar Caja</button>
-        
-        
-      </div>
+                <form class="form-inline">
+                    <div className="form-group">
+                        <label for="inputPassword6">Saldo Inicial de Caja</label>
+                        <input type="number" id="inputPassword6" class="form-control mx-sm-3" aria-describedby="passwordHelpInline" />
+                    </div>
+                    <div className="form-group">
+                        <label for="inputPassword6">Ventas en efectivo</label>
+                        <input type="number" id="inputPassword6" class="form-control mx-sm-3" aria-describedby="passwordHelpInline" />
+                    </div>
+                    <div className="form-group">
+                        <label for="inputPassword6">Saldo de Caja</label>
+                        <input type="number" id="inputPassword6" class="form-control mx-sm-3" aria-describedby="passwordHelpInline" />
+                    </div>
+                    <div className="form-group">
+                        <label for="inputPassword6">Ventas con Tarjeta</label>
+                        <input type="number" id="inputPassword6" class="form-control mx-sm-3" aria-describedby="passwordHelpInline" />
+                    </div>
+                </form>
+            </div>
+
+            <button type="button" className="btn btn-primary btn-lg caja">Cerrar Caja</button>
+
+
+        </div>
     </div>
-  )
+)
 
 export default CloseBox

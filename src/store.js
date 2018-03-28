1af7dd928ec92aed05d3997ce19c33939540e354
kamalyzl <kamaly.kazal@gmail.com>
2018-03-27 20:44:28 -0500

    version2

--- src/store.js
+++ src/store.js
@@ -1,25 +1,20 @@
 import { createStore } from 'redux';
 
-
 const reducer = (state, action) => {
-  switch (action.type) {
-    case "ADD_TO_CART":
-      return {
-        ...state,
-        cart: state.cart.concat(action.product)
-      }
-    case "REMOVE_FROM_CART":
-      return {
-        ...state,
-        cart: state.cart.filter(product => product.id !== action.product.id)
-      }
-    default:
-      return state;
 
-  }
+    if(action.type === "ADD_TO_CART"){
+
+        return {
+            ...state,
+            cart: state.cart.concat(action.product)
+        };
+
+    } else if (action.type === "REMOVE_FROM_CART"){
+        return{
+            ...state,
+            cart: state.cart.filter(product => product.id !== action.product.id)
+        };
+    }
+    return state;
 }
-
-const initialState = {
-  cart: []
-} 
-export default createStore(reducer,initialState);
+export default createStore (reducer, {cart:[]});
